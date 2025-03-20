5dd39782e3f7d6320f96d7bb7b33146dc8a3afbd
gramz <gramz@LAB4-CAT>
2025-03-20 09:23:14 -0600

    Se creo la clase para el manejo de archivos Json

---
+++ EmpleadoArchivo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace LaboratorioRepaso1
+{
+    internal class EmpleadoArchivo
+    {
+
+        public void Guardar (string archivo, List<Empleado> empleados)
+        {
+            string json = JsonConvert.SerializeObject(empleados);                        
+            System.IO.File.WriteAllText(archivo, json);
+        }
+
+        public List<Empleado> Leer (string archivo)
+        {           
+            List<Empleado> lista = new List<Empleado>();                        
+            StreamReader jsonStream = File.OpenText(archivo);
+            string json = jsonStream.ReadToEnd();            
+            jsonStream.Close();
+            
+            lista = JsonConvert.DeserializeObject<List<Empleado>>(json);
+            return lista;
+        }
+    }
+}
